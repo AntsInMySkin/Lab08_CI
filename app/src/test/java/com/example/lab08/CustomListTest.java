2e6ecaa1bec96b29575f9738dc2d4493e0192657
AntsInMySkin <leventozkan2003@gmail.com>
2025-10-28 17:53:31 -0600

    failing test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,21 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    private CustomList mockCityList(){
+        CustomList cityList = new CustomList();
+        cityList.addCity(mockCity());
+        return cityList;
+    }
 
+    private City mockCity(){
+        return new City("Edmonton", "Alberta");
+    }
+
+    @Test
+    public void mockHasCity(){
+        CustomList cityList = mockCityList();
+        City city = new City("Calgary", "Alberta");
+        cityList.addCity(city);
+        assertEquals(0,cityList.hasCity(city));
+    }
 }
